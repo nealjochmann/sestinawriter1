f529b3de0e085089594e06df8aa4b128847aa748
Neal Jochmann <nmjochmann@gmail.com>
2025-02-23 19:34:31 -0600

    move endwords to session

--- app.py
+++ app.py
@@ -8,31 +8,29 @@ app.config.from_object(__name__)
 Session(app)
 
 
-endwords = [] #TODO: move to session
 lines = [] #TODO: move to session
-
 
 @app.route("/")
 def index():
 	session["ew_counter"] = 0
-	return render_template("index.html", endwords=endwords, endwords_count=len(endwords))
+	return render_template("index.html", endwords_count=len(session["endwords"]))
 
 @app.route("/addword", methods=["POST"])
 def addWord():
 	word = request.form['word']
 	if len(word) > 0:
-		endwords.append(word)
+		session["endwords"].append(word)
 	return redirect(url_for("index"))
 
 @app.route("/clear", methods=["GET"])
 def clearWords():
-	endwords.clear()
+	session["endwords"].clear()
 	lines.clear()
 	return redirect(url_for("index"))
 
 @app.route("/write")
 def writeSestina():
-	return render_template("write.html", endwords=endwords, lines=lines, lineindex=session["ew_counter"])
+	return render_template("write.html", endwords=session["endwords"], lines=lines, lineindex=session["ew_counter"])
 
 @app.route("/addline", methods=["POST"])
 def addLine():
